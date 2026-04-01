adc42a150ddfe1e25c4856b97478c9f12127b9fc
Polvolox <144642400+Sp4ce-S4murai@users.noreply.github.com>
2026-04-01 10:42:32 -0300

    feat: generate static site output with new category pages, articles, and configuration updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
     site: 'https://oraculodigital.com',
-    output: 'server',
+    output: 'static',
     compressHTML: true,
     integrations: [
         mdx(),
         sitemap()
     ],
-    adapter: vercel(),
     vite: {
         css: {
             postcss: './postcss.config.mjs'
